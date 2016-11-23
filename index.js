8509c76734b7b1473be10d8c7194e2bb2856cf5d
Jyotman <jyotman94@gmail.com>
2016-11-23 19:18:16 +0530

    Add command dict play

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 var program = require('commander');
 var request = require('request');
+var co = require('co');
+var prompt = require('co-prompt');
 
 var API_KEY = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 var BASE_URL = 'http://api.wordnik.com/v4/word.json/';
@@ -35,16 +37,22 @@ program
 program
     .arguments('<word>')
     .description('All information about the word')
-    .action(all)
+    .action(all);
 
 program
     .command('dict <word>')
     .description('All information about the word')
-    .action(all)
+    .action(all);
+
+program
+    .command('play')
+    .description('Start a word game')
+    .action(play);
 
 console.log('\nLoading...\n');
 program.parse(process.argv);
 
+// Check if a specific command has been entered or not
 if (!commandFound) {
     wordOfTheDay();
 }
@@ -105,6 +113,61 @@ function wordOfTheDay() {
         var word = data.word;
         console.log('Word of the day - ' + word + '\n');
         all(word);
+    });
+}
+
+function play() {
+    var randomWordUrl = 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&api_key=' + API_KEY;
+    getWordData(randomWordUrl, function(data) {
+        var randomWord = data.word;
+        var synonymsUrl = BASE_URL + randomWord + '/relatedWords?relationshipTypes=synonym&api_key=' + API_KEY;
+        getWordData(synonymsUrl, function(data) {
+            // check if no synonyms found then get the meaning
+            if (!data[0] || data[0].words.length == 0) {
+                var url = BASE_URL + randomWord + '/definitions?limit=' + WORD_DEF_LIMIT + '&api_key=' + API_KEY;
+                getWordData(url, function(data) {
+                    getUserInput(randomWord, null, data[0].text);
+                });
+            } else {
+                var synonyms = data[0].words;
+                getUserInput(randomWord, synonyms);
+            }
+        });
+    });
+}
+
+function getUserInput(word, synonyms, meaning) {
+    if (synonyms)
+        console.log("A synonym of the word is " + word + "\n");
+    else
+        console.log("The meaning of the word is -\nMeaning: " + meaning);
+
+    co(function*() {
+        var ans = yield prompt('Enter the word: ');
+        if ((synonyms && synonyms.indexOf(ans.toLowerCase()) > -1) || (meaning && ans.toLowerCase() == word.toLowerCase())) {
+            console.log("\nCorrect answer!\n");
+            process.exit(0);
+        } else {
+            console.log('\nWrong answer!\n');
+            co(function*() {
+                var reply = yield prompt('Select a choice - \n1. Try again\n2. Exit\n')
+                if (reply == 1) {
+                    getUserInput(word, synonyms, meaning);
+                } else if (reply == 2) {
+                    if (meaning)
+                        console.log('The word was ' + word + '\n');
+                    else
+                        console.log('The word could have been any of these -\nSynonyms: ' + synonyms.join(",") + '\n');
+                    all(word);
+                    setTimeout(function() {
+                        process.exit(0);
+                    }, 5000);
+                } else {
+                    console.log('Incorrect input\n');
+                    process.exit(0);
+                }
+            });
+        }
     });
 }
 
